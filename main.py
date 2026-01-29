66be7c6abcdc381dcee83081a632b0b1ba3902dd
journeyhancock <journeyhancock480@gmail.com>
2026-01-29 12:29:20 -0700

    feat: serializing of hardware commands

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from liquid-sequence-scheduler!")
+from src.liquid_sequence_scheduler import parsing
+from src.liquid_sequence_scheduler import serializing
 
+def main():
+    sequence_list = parsing.parse_CSV("example/example_sheet.csv")
+    syntax_result = parsing.syntax_checking(sequence_list)
+    
+    if (not syntax_result.result):
+        print(f"Syntax Error: <{syntax_result.message}> at command number: {syntax_result.line}")
+        exit(-1)
+    else:
+        print("Syntax checking passed")
+        sequence = parsing.format_sequence(sequence_list)
+        parsing.print_sequence(sequence)
+
+    serial_sequence = serializing.serialize_sequence(sequence=sequence)
+    for node in serial_sequence:
+        print(node.pack_to_bytes())
+
+    # TODO make a connection and send the sequence 
 
 if __name__ == "__main__":
     main()
--- src/liquid_sequence_scheduler/serializing.py
+++ src/liquid_sequence_scheduler/serializing.py
@@ -1,21 +1,22 @@
 import struct
-import serial
 import zlib
 from dataclasses import dataclass, field
 from typing import List
 
-from classes import SequenceNode, Hardware, Command
+from src.liquid_sequence_scheduler.classes import SequenceNode, Hardware, Command
 
 @dataclass(frozen=True)
 class SerialNode:
-    time: bytes # 16 bits
-    opcode: bytes # 8 bits
-    sequence_num: bytes # 8 bits
+    time: float # 16 bits
+    subcommand_code: int # 8 bits
+    opcode: int # 8 bits
+    sequence_num: int # 8 bits
     crc: bytes = field(init=False) # 32 bits
 
     def __post_init__(self):
         payload = b"".join([
             struct.pack(">e", self.time),
+            struct.pack(">B", self.subcommand_code),
             struct.pack(">B", self.opcode),
             struct.pack(">B", self.sequence_num)
         ])
@@ -25,33 +26,47 @@ class SerialNode:
     def pack_to_bytes(self) -> bytes:
         return b"".join([
             struct.pack(">e", self.time),
+            struct.pack(">B", self.subcommand_code),
             struct.pack(">B", self.opcode),
             struct.pack(">B", self.sequence_num),
             struct.pack(">I", self.crc)
         ])
-    
-def float_to_time(value: float) -> bytes:
-    return struct.pack(">e", value)
 
-def parameter_to_opcode(parameter: Hardware | Command, switch: bool) -> bytes:
+def get_subcommand_code(parameter: Hardware | Command) -> int:
+    hardware_type = parameter.name
+    match hardware_type:
+        case "solenoid": return 81
+        case "valve": return 82
+
+        # default
+        case _: return 0
+
+def get_opcode(parameter: Hardware | Command, switch: bool) -> int:
     match parameter.name:
-        case "ox": return b"\x04" if switch else b"\x06"
-        case "fuel": return b"\x05" if switch else b"\x07"
-        case _: return b"\x00"
+        # solenoids
+        case "oxPress": return 1 if switch else 9
+        case "fuelPress": return 2 if switch else 16
+        case "oxVent": return 3 if switch else 17
+        case "fuelVent": return 4 if switch else 18
+        case "oxPurge": return 5 if switch else 19
+        case "fuelPurge": return 6 if switch else 20
 
-def serialize_sequence(sequence: List[SequenceNode]):
-    connection = serial.Serial(
-        port="",
-        baudrate=921600,
-        timeout=5
-    )
-    connection.open()
+        # valves
+        case "ox": return 4 if switch else 6
+        case "fuel": return 5 if switch else 7
 
+        # default
+        case _: return 0
+
+def serialize_sequence(sequence: List[SequenceNode]) -> List[SerialNode]:
+    serial_nodes: List[SerialNode] = []
     for node in sequence:
         new_event = SerialNode(
-            time=float_to_time(node.time),
-            opcode=parameter_to_opcode(node.parameter, node.switch),
-            sequence_num=node.num.to_bytes(1, "big"),
+            time=node.time,
+            subcommand_code=get_subcommand_code(node.parameter),
+            opcode=get_opcode(node.parameter, node.switch),
+            sequence_num=node.num
         )
-        connection.write(new_event.pack_to_bytes())
-        connection.reset_input_buffer()
+        serial_nodes.append(new_event)
+    
+    return serial_nodes
