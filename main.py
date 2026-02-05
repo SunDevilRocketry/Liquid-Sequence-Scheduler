6dcfd9e902573d912dbd87762728e7612b5abd6f
journeyhancock <journeyhancock480@gmail.com>
2026-02-05 16:47:21 -0700

    misc: label serial monitoring code

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ def main():
     sequence_list = parsing.parse_CSV("example/example_sheet.csv")
     syntax_result = parsing.syntax_checking(sequence_list)
     
+    # TODO ignore below, temporary serial monitoring setup 
     if (not syntax_result.result):
         print(f"Syntax Error: <{syntax_result.message}> at command number: {syntax_result.line}")
         exit(-1)
